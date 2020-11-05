ae7fd56d316aa5e7c15fae468a1e6248695bbef0
Daniel Unapanta <dunapanta2@gmail.com>
2020-11-04 19:21:14 -0600

    Change MenuBar for login and logout

--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -1,15 +1,36 @@
-import React, { useState } from 'react'
+import React, { useState, useContext } from 'react'
 import { Menu } from 'semantic-ui-react'
 import { Link } from 'react-router-dom'
 
+import { AuthContext } from '../context/auth'
+
 export default function MenuBar() {
+  const { user, logout } =useContext(AuthContext)
     const pathname = window.location.pathname;
     const path = pathname === '/' ? 'home' : pathname.substring(1);
     const [activeItem, setActiveItem] = useState(path);
 
     const handleItemClick = (e, { name }) => setActiveItem(name)
 
-    return (
+    const menuBar = user ? 
+      (
+        <Menu pointing secondary size="massive" color="teal">
+          <Menu.Item
+            name={`Hola ${user.username}`} 
+            active
+            as={Link}
+            to="/"
+          />
+          <Menu.Menu position='right'>
+            <Menu.Item
+                name='logout'
+                onClick={logout}
+            />
+          </Menu.Menu>
+        </Menu>
+      )
+      :
+      (
         <Menu pointing secondary size="massive" color="teal">
           <Menu.Item
             name='home'
@@ -35,6 +56,8 @@ export default function MenuBar() {
             />
           </Menu.Menu>
         </Menu>
-    )
+      )
+
+    return menuBar;
   
 }
