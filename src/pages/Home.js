7d0b06d2f267f1ea93de438822ecd16d292352cb
Daniel Unapanta <dunapanta2@gmail.com>
2020-11-10 11:12:40 -0600

    Adding transition on post

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react'
 import { useQuery } from '@apollo/client'
 import gql from 'graphql-tag'
-import { Grid } from 'semantic-ui-react'
+import { Grid, Transition } from 'semantic-ui-react'
 import { AuthContext } from '../context/auth'
 import PostCard from '../components/PostCard'
 import PostForm from '../components/PostForm'
@@ -22,11 +22,15 @@ export default function Home(){
                     </Grid.Column>
                 )}
                 { loading ? <h1>Loading</h1>
-                          : (posts && posts.map( post =>  ( 
-                    <Grid.Column key={post.id} style={{ marginBottom: 20 }}>
-                        <PostCard post={post} />
-                    </Grid.Column>)
-                )) }
+                          : (
+                              <Transition.Group>
+                                  {posts && posts.map( post =>  ( 
+                                    <Grid.Column key={post.id} style={{ marginBottom: 20 }}>
+                                        <PostCard post={post} />
+                                    </Grid.Column>)
+                                    )}
+                              </Transition.Group>
+                )}
             </Grid.Row>
         </Grid>
     )
